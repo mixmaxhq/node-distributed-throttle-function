6c5dcf8479a6c613e782022cfbf9450550901317
Eli Skeggs <eli@mixmax.com>
2020-02-03 12:51:30 -0800

    style: apply prettier

--- index.js
+++ index.js
@@ -7,29 +7,28 @@ function makeRedisKey(key) {
 module.exports = function(redis) {
   var scripty = new Scripty(redis);
 
-  var throttle = function(key, throttleFn, ttl, noRetry /* Private */ ) {
-    scripty.loadScriptFile(
-      'claimThrottleLock',
-      __dirname + '/lua/claimThrottleLock.lua',
-      function(err, claimThrottleLock) {
+  var throttle = function(key, throttleFn, ttl, noRetry /* Private */) {
+    scripty.loadScriptFile('claimThrottleLock', __dirname + '/lua/claimThrottleLock.lua', function(
+      err,
+      claimThrottleLock
+    ) {
+      if (err) return throttleFn(err);
+
+      claimThrottleLock.run(1, makeRedisKey(key), ttl, function(err, expiresIn) {
         if (err) return throttleFn(err);
 
-        claimThrottleLock.run(1, makeRedisKey(key), ttl, function(err, expiresIn) {
-          if (err) return throttleFn(err);
-
-          // If being throttled, wait for its expiration and try to run once more.
-          if (expiresIn >= 0) {
-            if (!noRetry) {
-              setTimeout(function() {
-                throttle(key, throttleFn, ttl, true /* No retry */ );
-              }, expiresIn);
-            }
-          } else {
-            throttleFn();
+        // If being throttled, wait for its expiration and try to run once more.
+        if (expiresIn >= 0) {
+          if (!noRetry) {
+            setTimeout(function() {
+              throttle(key, throttleFn, ttl, true /* No retry */);
+            }, expiresIn);
           }
-        });
-      }
-    );
+        } else {
+          throttleFn();
+        }
+      });
+    });
   };
 
   // Create an alias.
